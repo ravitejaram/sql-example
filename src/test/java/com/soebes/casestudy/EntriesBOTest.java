82ebe0cbbed7e6bb17394418be1d443ff1c9a015
Karl Heinz Marbaise <info@soebes.de>
2013-06-08 17:40:53 +0200

    - Changed to EntityManager.

--- src/test/java/com/soebes/casestudy/EntriesBOTest.java
+++ src/test/java/com/soebes/casestudy/EntriesBOTest.java
@@ -5,20 +5,42 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.TimeZone;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaQuery;
+
 import org.apache.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.google.common.base.Joiner;
 import com.soebes.casestudy.bo.EntriesBO;
-import com.soebes.casestudy.dao.DAOFactory;
-import com.soebes.casestudy.dao.IdDAO;
 
 public class EntriesBOTest extends BOTestBase {
     private static Logger LOGGER = Logger.getLogger(EntriesBOTest.class);
 
+    private EntityManagerFactory emf;
+
+    private EntityManager em;
+
     @BeforeClass
     public void beforeClass() {
+        emf = Persistence.createEntityManagerFactory("hibernate-search-example");
+
+        // EntityManagerFactory emf =
+        // cfg.addProperties( properties ) //add some properties
+        // .setInterceptor( myInterceptorImpl ) // set an interceptor
+        // .addAnnotatedClass( MyAnnotatedClass.class ) //add a class to be
+        // mapped
+        // .addClass( NonAnnotatedClass.class ) //add an hbm.xml file using the
+        // Hibernate convention
+        // .addRerousce( "mypath/MyOtherCLass.hbm.xml ) //add an hbm.xml file
+        // .addRerousce( "mypath/orm.xml ) //add an EJB3 deployment descriptor
+        // .configure("/mypath/hibernate.cfg.xml") //add a regular
+        // hibernate.cfg.xml
+        // .buildEntityManagerFactory(); //Create the entity manager factory
+        em = emf.createEntityManager();
         LOGGER.debug("beforeClass()");
 
         LOGGER.debug("beforeClass(done)");
@@ -28,8 +50,10 @@ public class EntriesBOTest extends BOTestBase {
 
     @Test(enabled = true)
     public void testGet() {
-        IdDAO<EntriesBO> dao = DAOFactory.getEntries();
-        List<EntriesBO> resultList = dao.get();
+        CriteriaQuery<EntriesBO> criteria = em.getCriteriaBuilder().createQuery(EntriesBO.class);
+        criteria.from(EntriesBO.class);
+        List<EntriesBO> resultList = em.createQuery(criteria).getResultList();
+        
         LOGGER.info("Number of entries:" + resultList.size());
         for (EntriesBO entriesBO : resultList) {
             Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT-2:00"));
